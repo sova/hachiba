3b642b66e6703db338b4c842e415e9eb49d0e3cf
vas <sova.kuliana@gmail.com>
2018-06-23 01:13:12 -0600

    atoms added

--- src/hachiba/handler.clj
+++ src/hachiba/handler.clj
@@ -5,6 +5,26 @@
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (def page (atom {:user "vaso"}))
+(def page-names (atom {:pagename "page_id"}))
+(def page-threads (atom {:pagename ["thread_ids"]})) ; ordered based on thread stats
+(def page-terms (atom {:pagename ["terms"]}))
+(def threads (atom {:thread_id ["post_ids"]}))
+(def posts (atom {:post_id "post_id"
+                   :contents "content"
+                   :timestamp 1018
+                   :num_posts 1
+                   :last-touched 1018}))
+
+(defn page-for-post
+  "Given a post id, return the pagename"
+  [post_id]
+  (str "stetcher" post_id))
+(defn thread-for-post [] )
+(defn post-for-query [] )
+(defn terms-for-page [] )
+
+
+
 (def cm { :component/header (html [:div#heading.top
                                    [:span "hachiba 蜂場 はちば"]
                                    [:div#menu.top "Hachiba"]])
@@ -19,17 +39,31 @@
                                  [:div "logout"]
                                  [:div "complete index"]]])
 
+          :component/search (html [:div#searchbar.main
+                                   [:div "practicalhuman.org/"
+                                    [:input#searchbox]]])
+
           :component/body (html (let [coll [1 2 3 4 5 6 7]]
                             [:body
                             [:link {:type "text/css", :href "css/hachiba.css", :rel "stylesheet"}]
+                            [:br][:br]
                             [:p
-                            [:span {:class "HelloBox"} "Hello!"]
-                            [:ul (for [x coll] [:li x])]]]))})
+                            [:div {:class "HelloBox"} "Hello!"]
+                            [:ul (for [x coll] [:li x])]]]))
+
+          :component/footer (html [:div#footing "top folds"
+                                   [:div#topfolds.main
+                                    [:div "/nature"]
+                                    [:div "/wigwam"]
+                                    [:div "/jokes"]]])
+          })
 
 (defroutes app-routes
-  (GET "/" [] (concat (:component/header cm)
+  (GET "/" [] (concat (:component/search cm)
+                      (:component/header cm)
                       (:component/menu cm)
-                      (:component/body cm)))
+                      (:component/body cm)
+                      (:component/footer cm)))
   (GET "/hello" [] (str "Hello person! Welcome to Hachiba!"))
   (GET "/:x" [x] (str x))
   (route/not-found "Not Found"))
