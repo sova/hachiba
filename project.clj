655c8e3ccdfa1df2a3da9aa0c82da24215495489
vas <sova.kuliana@gmail.com>
2018-06-22 18:53:14 -0600

    start some
    hachiba handlers start

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://nonforum.com/hachiba"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [compojure "1.5.1"]
+                 [trail "2.1.0"]
                  [ring/ring-defaults "0.2.1"]
-                 [funcool/bide "1.6.0"] ;faster routing than compojure
                  [hiccup "1.0.5"]
                  ]
   :plugins [[lein-ring "0.9.7"]]
-  :ring {:handler hachiba.handler/app}
+  :ring {:handler hachiba.handler/app
+         :port 1337}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.0"]]}})
--- src/hachiba/handler.clj
+++ src/hachiba/handler.clj
@@ -4,14 +4,38 @@
             [hiccup.core :refer :all]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def page (atom {:user "vaso"}))
+(def cm { :component/header (html [:div#heading.top
+                                   [:span "hachiba 蜂場 はちば"]
+                                   [:div#menu.top "Hachiba"]])
+          :component/menu (html [:div#menu.main
+                                 [:div.hidden
+                                 [:div (str "logged in as: " (:user @page))]
+                                 [:div "points"]
+                                 [:div "top page"]
+                                 [:div "fresh"]
+                                 [:div "submit"]
+                                 [:div "favorites"]
+                                 [:div "logout"]
+                                 [:div "complete index"]]])
+
+          :component/body (html (let [coll [1 2 3 4 5 6 7]]
+                            [:body
+                            [:link {:type "text/css", :href "css/hachiba.css", :rel "stylesheet"}]
+                            [:p
+                            [:span {:class "HelloBox"} "Hello!"]
+                            [:ul (for [x coll] [:li x])]]]))})
+
 (defroutes app-routes
-  (GET "/" []
-       (let [coll [1 2 3 4 5 6 7]]
-             (html [:p
-                     [:span {:class "HelloBox"} "Hello!"]
-                     [:ul (for [x coll] [:li x])]]))))
-  (GET "/" []
+  (GET "/" [] (concat (:component/header cm)
+                      (:component/menu cm)
+                      (:component/body cm)))
+  (GET "/hello" [] (str "Hello person! Welcome to Hachiba!"))
+  (GET "/:x" [x] (str x))
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes site-defaults))
+
+
+
